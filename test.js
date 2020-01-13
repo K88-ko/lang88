68cc3e9ff76cc5e62bdd8557379a0f17222e9ec1
K88-ko <59752477+K88-ko@users.noreply.github.com>
2020-01-13 22:17:45 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,10 @@
 // external js: isotope.pkgd.js
 
-
 // init Isotope
 var $grid = $('.grid').isotope({
   itemSelector: '.element-item',
-  layoutMode: 'fitRows',
-  getSortData: {
-    name: '.name',
-    symbol: '.symbol',
-    number: '.number parseInt',
-    category: '[data-category]',
-    weight: function( itemElem ) {
-      var weight = $( itemElem ).find('.weight').text();
-      return parseFloat( weight.replace( /[\(\)]/g, '') );
-    }
-  }
+  layoutMode: 'fitRows'
 });
-
 // filter functions
 var filterFns = {
   // show if number is greater than 50
@@ -30,21 +18,13 @@ var filterFns = {
     return name.match( /ium$/ );
   }
 };
-
 // bind filter button click
-$('#filters').on( 'click', 'button', function() {
+$('.filters-button-group').on( 'click', 'button', function() {
   var filterValue = $( this ).attr('data-filter');
   // use filterFn if matches value
   filterValue = filterFns[ filterValue ] || filterValue;
   $grid.isotope({ filter: filterValue });
 });
-
-// bind sort button click
-$('#sorts').on( 'click', 'button', function() {
-  var sortByValue = $(this).attr('data-sort-by');
-  $grid.isotope({ sortBy: sortByValue });
-});
-
 // change is-checked class on buttons
 $('.button-group').each( function( i, buttonGroup ) {
   var $buttonGroup = $( buttonGroup );
